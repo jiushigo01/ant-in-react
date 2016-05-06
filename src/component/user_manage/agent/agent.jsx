9a2b5ac10ea518dacdd72419cc4b997b54e99b36
WangHui <346960399@qq.com>
2016-05-06 17:18:21 +0800

    Tables组件

--- src/component/user_manage/agent/agent.jsx
+++ src/component/user_manage/agent/agent.jsx
@@ -52,23 +52,27 @@ const options = [
 	];
 
 
-const columns = [{
-  title: '姓名',
-  dataIndex: 'name',
-  filters: [
-    { text: '姓李的', value: '李' },
-    { text: '姓胡的', value: '胡' },
-  ],
-}, {
-  title: '年龄',
-  dataIndex: 'age',
-  sorter: true,
-}, {
-  title: '住址',
-  dataIndex: 'address',
-}];
+const columns = [
+		{
+		  	title: '姓名',
+		  	dataIndex: 'name',
+		  	filters: [
+		    	{ text: '姓李的', value: '李' },
+		    	{ text: '姓胡的', value: '胡' },
+		   ],
+		},
+		{
+		  	title: '年龄',
+		  	dataIndex: 'age',
+		  	sorter: true,
+		},
+		{
+		    title: '住址',
+		  	dataIndex: 'address',
+		}
+	];
 
-const tables = React.createClass({
+const Tables = React.createClass({
   getInitialState() {
     return {
       data: [],
@@ -87,7 +91,6 @@ const tables = React.createClass({
       currentPage: pagination.current,
       sortField: sorter.field,
       sortOrder: sorter.order,
-      ...filters,
     });
   },
   fetch(params = {}) {
@@ -97,10 +100,11 @@ const tables = React.createClass({
       url: 'http://ant.design/components/table/demo/data.json',
       method: 'get',
       data: params,
-      type: 'json',
+      type: 'jsonp',
       success: (result) => {
+      	console.log(result);
         const pagination = this.state.pagination;
-        pagination.total = result.totalCount;
+        pagination.total = result.data.length;
         this.setState({
           loading: false,
           data: result.data,
@@ -123,8 +127,6 @@ const tables = React.createClass({
   }
 });
 
-// ReactDOM.render(<Test />, document.getElementById('test'));
-
 const agent = React.createClass({
   getInitialState() {
     return {
@@ -139,100 +141,95 @@ const agent = React.createClass({
   },
   render() {
 	return (
-		<Form horizontal className="advanced-search-form">
-		  <Row>
-		    <Col span="8">
-		      <FormItem
-		        label="注册手机号："
-		        labelCol={{ span: 6 }}
-		        wrapperCol={{ span: 14 }}>
-		        <Input />
-		      </FormItem>
-		      <FormItem
-		        label="注册倾向："
-		        labelCol={{ span: 6 }}
-		        wrapperCol={{ span: 18 }}>
-		        <RadioGroup onChange={this.onChange} value={this.state.value}>
-			        <Radio key="a" value={1}>不限</Radio>
-			        <Radio key="b" value={2}>投广告</Radio>
-			        <Radio key="c" value={3}>接广告</Radio>
-			        <Radio key="d" value={null}>未确定</Radio>
-			    </RadioGroup>
-		      </FormItem>
-		      <FormItem
-		        label="推广需求："
-		        labelCol={{ span: 6 }}
-		        wrapperCol={{ span: 14 }}>
-		        <Input placeholder="请输入关键字" />
-		      </FormItem>
-		    </Col>
-		    <Col span="8">
-		      <FormItem
-		        label="注册时间："
-		        labelCol={{ span: 6 }}
-		        wrapperCol={{ span: 18 }}>
-		        <RangePicker style={{ width: 184 }} />
-		      </FormItem>
-		      <FormItem
-		        label="是否媒体："
-		        labelCol={{ span: 6 }}
-		        wrapperCol={{ span: 18 }}>
-		        <RadioGroup>
-			        <Radio key="a" value={4}>不限</Radio>
-			        <Radio key="b" value={5}>是</Radio>
-			        <Radio key="c" value={6}>否</Radio>
-			        <Radio key="d" value={7}>审核中</Radio>
-			    </RadioGroup>
-		      </FormItem>
-		      <FormItem
-		        label="跟进人员："
-		        labelCol={{ span: 6 }}
-		        wrapperCol={{ span: 18 }}>
-		        <Cascader options={options} />
-		      </FormItem>
-		    </Col>
-		    <Col span="8">
-		      <FormItem
-		        label="绑定微信号："
-		        labelCol={{ span: 6 }}
-		        wrapperCol={{ span: 14 }}>
-		        <Input placeholder="请输入微信昵称" />
-		      </FormItem>
-		      <FormItem
-		        label="较长搜索名称："
-		        labelCol={{ span: 6 }}
-		        wrapperCol={{ span: 18 }}>
-		        <Select defaultValue="unlimit" style={{ width: 120 }}>
-			      	<Option value="unlimit">不限</Option>
-			      	<Option value="agent1">代理商1</Option>
-			      	<Option value="agent2">代理商2</Option>
-			      	<Option value="agent3">代理商3</Option>
-			      	<Option value="agent4">代理商4</Option>
-			      	<Option value="agent5">代理商5</Option>
-			      	<Option value="agent6">代理商6</Option>
-			    </Select>
-		      </FormItem>
-		      <FormItem
-		      	label=" "
-		      	labelCol={{ span: 6 }}
-		        wrapperCol={{ span: 18 }}>
-		        <Button type="primary" htmlType="submit">搜索</Button>
-		      </FormItem>
-		    </Col>
-		  </Row>
-		</Form>
+		<div>
+			<Form horizontal className="advanced-search-form">
+			  <Row>
+			    <Col span="8">
+			      <FormItem
+			        label="注册手机号："
+			        labelCol={{ span: 6 }}
+			        wrapperCol={{ span: 14 }}>
+			        <Input />
+			      </FormItem>
+			      <FormItem
+			        label="注册倾向："
+			        labelCol={{ span: 6 }}
+			        wrapperCol={{ span: 18 }}>
+			        <RadioGroup onChange={this.onChange} value={this.state.value}>
+				        <Radio key="a" value={1}>不限</Radio>
+				        <Radio key="b" value={2}>投广告</Radio>
+				        <Radio key="c" value={3}>接广告</Radio>
+				        <Radio key="d" value={null}>未确定</Radio>
+				    </RadioGroup>
+			      </FormItem>
+			      <FormItem
+			        label="推广需求："
+			        labelCol={{ span: 6 }}
+			        wrapperCol={{ span: 14 }}>
+			        <Input placeholder="请输入关键字" />
+			      </FormItem>
+			    </Col>
+			    <Col span="8">
+			      <FormItem
+			        label="注册时间："
+			        labelCol={{ span: 6 }}
+			        wrapperCol={{ span: 18 }}>
+			        <RangePicker style={{ width: 184 }} />
+			      </FormItem>
+			      <FormItem
+			        label="是否媒体："
+			        labelCol={{ span: 6 }}
+			        wrapperCol={{ span: 18 }}>
+			        <RadioGroup>
+				        <Radio key="a" value={4}>不限</Radio>
+				        <Radio key="b" value={5}>是</Radio>
+				        <Radio key="c" value={6}>否</Radio>
+				        <Radio key="d" value={7}>审核中</Radio>
+				    </RadioGroup>
+			      </FormItem>
+			      <FormItem
+			        label="跟进人员："
+			        labelCol={{ span: 6 }}
+			        wrapperCol={{ span: 18 }}>
+			        <Cascader options={options} />
+			      </FormItem>
+			    </Col>
+			    <Col span="8">
+			      <FormItem
+			        label="绑定微信号："
+			        labelCol={{ span: 6 }}
+			        wrapperCol={{ span: 14 }}>
+			        <Input placeholder="请输入微信昵称" />
+			      </FormItem>
+			      <FormItem
+			        label="较长搜索名称："
+			        labelCol={{ span: 6 }}
+			        wrapperCol={{ span: 18 }}>
+			        <Select defaultValue="unlimit" style={{ width: 120 }}>
+				      	<Option value="unlimit">不限</Option>
+				      	<Option value="agent1">代理商1</Option>
+				      	<Option value="agent2">代理商2</Option>
+				      	<Option value="agent3">代理商3</Option>
+				      	<Option value="agent4">代理商4</Option>
+				      	<Option value="agent5">代理商5</Option>
+				      	<Option value="agent6">代理商6</Option>
+				    </Select>
+			      </FormItem>
+			      <FormItem
+			      	label=" "
+			      	labelCol={{ span: 6 }}
+			        wrapperCol={{ span: 18 }}>
+			        <Button type="primary" htmlType="submit">搜索</Button>
+			      </FormItem>
+			    </Col>
+			  </Row>
+			</Form>
+			<Tables />
+		</div>
 	)
   }
 });
 
-// const agents = React.createClass({
-// 	render:function(){
-// 		return (
-// 			<searchform />
-// 			<Test />
-// 		)
-// 	}
-// })
 
 
 export default agent;
